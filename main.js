b1f511d7914c547923ad7f30c8ba5caa4e9ba640
Tact <69988786+ItsTact@users.noreply.github.com>
2020-09-18 08:02:16 -0400

    Update main.js

--- main.js
+++ main.js
@@ -10,7 +10,7 @@ client.login('NzUyNjMyNDgzMDM1NDgwMjI0.X1adnw.Svr759s0nffqRMPDZwUG_NuBq1c');
 client.on('message', message => {
     if (message.content === '?solus' || message.content === '?info') {
 
-		message.channel.send('`SolusMC is a Cracked Minecraft server founded by @KhosrawAzizi#5634 and it was released for the first time on August 25, 2020. Ever since, our server\'s player base is intermittently growing since it\'s first released, users around all over the world playing our amazing gamemodes and enjoying their time here with our beloved community. Can\'t wait to play? Join the community today!`');
+	message.channel.send('`SolusMC is a Cracked Minecraft server founded by @KhosrawAzizi#5634 and it was released for the first time on August 25, 2020. Ever since, our server\'s player base is intermittently growing since it\'s first released, users around all over the world playing our amazing gamemodes and enjoying their time here with our beloved community. Can\'t wait to play? Join the community today!`');
 
     } else if (message.content === '?ip') { /** Condition for IP Command */
 
@@ -18,7 +18,7 @@ client.on('message', message => {
 
     } else if (message.content === '?rules') { /** Condition for IP Command */
 
-        message.channel.send('Please read #rules for all the rules!');
+        message.channel.send('Please read the Discord rules at #rules. Server/Network Rules are located at https://www.solusmc.net/forums/forum/topic/9-network-rules');
 
     } else if (message.content === '?forums') { /** Condition for Forums Link Command */
 
@@ -31,27 +31,27 @@ client.on('message', message => {
     } else if (message.content === '?discord') { /** Condition for Discord Invite Link Command */
 
         message.channel.send('`invite.gg/solusmc`');
+	    
+    } else if (message.content === '?store') { /** Condition for Store Link Command */
+
+        message.channel.send('`Please consider making a donation at https://store.solusmc.net!`');
 
     } else if (message.content === '?apply') { /** Condition for Application Info Command */
 
-        message.channel.send('Please see https://www.solusmc.net/forums/forum/view/17-applications-and-appeals/ for more information regarding applying for staff, builder, or YouTuber, and appealing a ban or blacklist. Please read the guidelines and requirements before you apply!');
+        message.channel.send('Please see https://www.solusmc.net/forums/forum/view/17-applications-and-appeals/ for more information regarding applying for staff, builder, and appealing a ban or blacklist. Please read the guidelines and requirements before you apply!');
 
     } else if (message.content === '?staff') { /** Condition for Staff Roles Command */
 
-        message.channel.send('`Owner, Admin, Senior Mod, Moderator, Trainee`');
-
-    } else if (message.content === '?smod') { // Condition for SMods List Command
-
-        message.channel.send('`HurtCry`');
-
-    } else if (message.content === '?admin') { // Condition for Admin List Command
-
-        message.channel.send('`Tact, georgieHH`');
+        message.channel.send('https://www.solusmc.net/staff.html');
 
     } else if (message.content === '?help') { // Condition for Bot Help Command
 
-        message.channel.send('`?help -> All Commands With Description\n?ip -> Server IP Address\n?forums -> Forums Link\n?website -> Website Link\n?apply -> Application Info\n?staff -> All Staff Ranks`');
+        message.channel.send('`?help -> All Commands With Description\n\n?info -> Server Information\n?ip -> Server IP Address\n?rules -> Rules for the server and Discord\n?forums -> Forums Link\n?website -> Website Link\n?discord -> Discord Invite Link\n?store -> Server Store for buying Ranks and upgrades\n?apply -> Application Info\n?staff -> Link to Staff Page\n?vote -> Link to Voting Links`');
 
+    } else if (message.content === '?vote') { /** Condition for Vote Link Command */
+
+        message.channel.send('`Please consider supporting the server at the links at https://www.solusmc.net/vote.html. It helps out a ton if you vote!`');
+	    
     } else if (message.content === '?owner') { // Condition for Owner Name Command
 
         message.channel.send('`The owner of this server is Khosraw Azizi. He a cutie! <3`');
@@ -63,7 +63,11 @@ client.on('message', message => {
 
     } else if (message.content === 'Happy now?') {
 
-        message.channel.send('`Thanks cutie! I am happy now!`');
+        message.channel.send('`Thanks! I am happy now!`');
+	    
+    } else if (message.content === ':(') {
+
+        message.channel.send('`Cheer up buddy!`');
 
     }
 
